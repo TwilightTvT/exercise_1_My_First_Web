7b352e11829029fa9576be4a3ea100b36b507e38
TwilightTVT <1416813305@qq.com>
2022-04-22 20:35:36 +0800

    11

--- JavaScript2.js
+++ JavaScript2.js
@@ -1,6 +1,6 @@
 function inputAnwser(){
- var response= prompt("请问ABCD的面积是多少？(不需要单位）");
- if(response.value =40){
+ var response = prompt("请问ABCD的面积是多少？(不需要单位）");
+ if (response === "40"){
 				alert("恭喜你，"+ response +"平方厘米是正确答案！");
       }else{
 				alert("很遗憾，"+ response +"平方厘米不是正确答案，需要提示请点击下方按钮。");
@@ -16,4 +16,8 @@ function showTopic(){
   x.style.foutSize="25xp";
   x.style.color="white";
   x.innerHTML = "剩余提示0/1";
+}
+
+function rightAnwser(){
+   alert("正确答案是40。");
 }
