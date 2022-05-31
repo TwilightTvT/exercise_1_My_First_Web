d8842817e4da24cb16965f035cd6bea6f14afd06
TwilightTVT <1416813305@qq.com>
2022-05-31 13:31:51 +0800

    111

---
+++ final2/script.js
@@ -0,0 +1,80 @@
+const title= document.querySelector('.title');
+const prev= document.querySelector('.prev');
+const playPause= document.querySelector('.playPause');
+const next= document.querySelector('.next');
+const audio= document.querySelector('audio');
+
+// song list
+const songList =[
+    {
+        path:"music/Billie Eilish - bad guy.mp3",
+        songName:"Bad guy",
+    },
+    {
+        path:"music/Billie Eilish - everything i wanted.mp3",
+        songName:"Everything i wanted",
+    },
+
+    {
+        path:"music/Billie Eilish - Ocean Eyes.mp3",
+        songName:"Ocean Eyes",
+    },
+    {
+        path:"music/Billie Eilish - hostage.mp3",
+        songName:"Hostage",
+    },
+];
+
+let songPlaying = false;
+
+//play song
+ const playSong = ()=>{
+    songPlaying = true;
+    audio.play();
+    playPause.classList.add('active');
+    playPause.innerHTML = '<span class="material-icons">pause</span>';
+}
+
+//pause song
+const pauseSong = () => {
+    songPlaying = false;
+    audio.pause();
+    playPause.classList.remove('active');
+    playPause.innerHTML = ' <span class="material-icons">play_arrow</span>';
+
+}
+//play/pause song
+playPause.addEventListener("click",() => (songPlaying? pauseSong() : playSong()));
+
+//load song
+const loadSong = (songList) => {
+    title.textContent = songList.songName;
+    audio.src = songList.path;//
+
+}
+let i = 0;
+loadSong (songList[i]);
+
+//previous song
+const prevSong = () => {
+    i--;
+    if (i < 0){
+        i = songList.length -1;
+    }
+    loadSong(songList[i]);
+    playSong();
+}
+
+prev.addEventListener("click", prevSong);
+
+//next song
+const nextSong = () => {
+    i++;
+    if (i > (songList.length - 1)){
+        i = 0;
+    }
+    loadSong(songList[i]);
+    playSong();
+}
+
+next.addEventListener("click", nextSong);
---
+++ 简约播放器/script.js
@@ -0,0 +1,78 @@
+const title= document.querySelector('.title');
+const prev= document.querySelector('.prev');
+const playPause= document.querySelector('.playPause');
+const next= document.querySelector('.next');
+const audio= document.querySelector('audio');
+
+// song list
+const songList =[
+    {
+        // 你的音乐的路径,下面两个xxx处同理
+        path:"She Is My Sin.mp3",
+        // 你的音乐的名字，下面两个xxx处同理
+        songName:"She Is My Sin",
+    },
+    {
+        path:"xxxxxx",
+        songName:"xxxx",
+    },
+
+    {
+        path:"xxx",
+        songName:"xxx",
+    },
+];
+
+let songPlaying = false;
+
+//play song
+ const playSong = ()=>{
+    songPlaying = true;
+    audio.play();
+    playPause.classList.add('active');
+    playPause.innerHTML = '<span class="material-icons">pause</span>';
+}
+
+//pause song
+const pauseSong = () => {
+    songPlaying = false;
+    audio.pause();
+    playPause.classList.remove('active');
+    playPause.innerHTML = ' <span class="material-icons">play_arrow</span>';
+
+}
+//play/pause song
+playPause.addEventListener("click",() => (songPlaying? pauseSong() : playSong()));
+
+//load song
+const loadSong = (songList) => {
+    title.textContent = songList.songName;
+    audio.src = songList.path;//
+
+}
+let i = 0;
+loadSong (songList[i]);
+
+//previous song
+const prevSong = () => {
+    i--;
+    if (i < 0){
+        i = songList.length -1;
+    }
+    loadSong(songList[i]);
+    playSong();
+}
+
+prev.addEventListener("click", prevSong);
+
+//next song
+const nextSong = () => {
+    i++;
+    if (i > (songList.length - 1)){
+        i = 0;
+    }
+    loadSong(songList[i]);
+    playSong();
+}
+
+next.addEventListener("click", nextSong); 
